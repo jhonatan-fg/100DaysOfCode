13a92333a8229552492e17eba1e8fa1543d538b2
jhonatan <jhonatan@protonmail.ch>
2021-05-12 00:48:54 -0500

    dia 18Ã9

---
+++ Javascript/Junior/metodos-de-cadena/index.js
@@ -0,0 +1,30 @@
+//METODO DE CADENAS
+
+let cadena = "cadena de prueba"
+let cadena2 = "cadena "
+
+resultado = cadena.repeat(10)
+//endWith = termina igual
+//startsWith = empieza igual
+//includes = incluye esto
+//indexOf = en que posicion comienza
+//lastIndexOf = muestra la posicion de la ultima coincidencia
+//padStart
+//padEnd
+//repeat
+//split, divide la cadena como le digamos
+//substring = retorna un pedazo de cadena
+//tolowerCase = convierte  minuscula
+//toupperCase = convierte a mayuscula
+//toString = convierte a string
+//trim = elimina los espacios en blanco
+//trimEnd = elimina espacios al final
+//trimStart = elimina espacios al inicio
+//valueOf = 
+//
+
+
+document.write(resultado)
+
+
+
